f10fcae44e06d91d0b32e13a97a56f73acc139de
kis9a <kis9ax@gmail.com>
2022-08-07 18:35:40 +0900

    refactor

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net"
@@ -62,13 +64,84 @@ var (
 	Version = "0.0.0"
 )
 
-const dirTmpl = `
+// server
+func newServer(dir string) (*Server, error) {
+	// set server dir
+	server := &Server{
+		Directory: dir,
+	}
+
+	// check tcp addr
+	addr := net.JoinHostPort(options.ServerHost, options.ServerPort)
+	_, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return server, fmt.Errorf("Listen addr could not resolve: %s", addr)
+	} else {
+		server.Addr = addr
+	}
+
+	// iniitalize timeh
+	timeh, err := newTimeHelper(options.LocationName)
+	if err != nil {
+		return server, fmt.Errorf("failed initialize time helper: %v", err)
+	} else {
+		server.TimeHelper = timeh
+	}
+
+	return server, err
+}
+
+func (s *Server) serve() error {
+	http.HandleFunc("/", s.routeHandler)
+
+	log.Printf("Serving %s at %s", s.Directory, s.Addr)
+
+	if options.Open {
+		err := browserOpen("http://" + s.Addr)
+		if err != nil {
+			os.Stderr.WriteString("failed browser open: " + err.Error())
+		}
+	}
+	return http.ListenAndServe(s.Addr, nil)
+}
+
+func (s *Server) routeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s: %s %s", r.RemoteAddr, r.Method, r.Host+r.RequestURI)
+
+	if r.Method == http.MethodGet {
+		s.handler(w, r)
+	}
+}
+
+func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "no-store")
+
+	urlPath, err := url.PathUnescape(r.URL.Path)
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("failed to path unescape request path %v", err))
+		http.Error(w, fmt.Sprintf("failed to path unescape request path %v", err), http.StatusInternalServerError)
+		return
+	}
+	s.Path = path.Join(s.Directory, urlPath)
+	s.Query = r.URL.Query()
+
+	if !isExistPath(s.Path) {
+		os.Stderr.WriteString(fmt.Sprintf("No such file or directory %s", s.Path))
+		http.Error(w, "file not found", http.StatusNotFound)
+		return
+	}
+
+	s.render(w, r)
+}
+
+func (s *Server) renderBase(w http.ResponseWriter) error {
+	const baseTmpl = `
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=dev ice-width, initial-scale=1.0" />
-    <title>fs</title>
+    <title>fs {{ .Path }}</title>
     <link rel="icon" href="data:," />
     <style>
       * {
@@ -100,68 +173,107 @@ const dirTmpl = `
 </html>
 `
 
-// server
-func newServer(dir string) (*Server, error) {
-	server := &Server{
-		Directory: dir,
+	type Template struct {
+		Path string
 	}
 
-	addr := net.JoinHostPort(options.ServerHost, options.ServerPort)
-	_, err := net.ResolveTCPAddr("tcp", addr)
+	tmpl, err := template.New("").Parse(baseTmpl)
 	if err != nil {
-		return server, fmt.Errorf("Listen addr could not resolve: %s", addr)
-	} else {
-		server.Addr = addr
+		return err
 	}
 
-	timeh, err := newTimeHelper(options.LocationName)
-	if err != nil {
-		return server, fmt.Errorf("failed initialize time helper: %v", err)
-	} else {
-		server.TimeHelper = timeh
-	}
+	var b bytes.Buffer
+	tmpl.Execute(&b, Template{
+		Path: s.Path,
+	})
 
-	return server, err
+	_, err = io.WriteString(w, b.String())
+	return err
 }
 
-func (s *Server) serve() error {
-	http.HandleFunc("/", s.routeHandler)
-	log.Printf("Serving %s at %s", s.Directory, s.Addr)
-	if options.Open {
-		err := browserOpen("http://" + s.Addr)
-		if err != nil {
-			os.Stderr.WriteString("failed browser open: " + err.Error())
+func (s *Server) renderDirectory(f *os.File, w http.ResponseWriter) error {
+	html, err := os.Open(path.Join(s.Path, "index.html"))
+
+	if err == nil {
+		io.Copy(w, html)
+		os.Stderr.WriteString(fmt.Sprintf("failed write index.html %v", err))
+		html.Close()
+		return err
+	}
+	html.Close()
+
+	files, err := s.readDirectoryFiles(f)
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("failed read dir files %v", err))
+		http.Error(w, fmt.Sprintf("failed read dir files %v", err), http.StatusInternalServerError)
+		return err
+	}
+
+	absDir, err := filepath.Abs(s.Directory)
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("Could not get directory absolute dir %v", err))
+		http.Error(w, fmt.Sprintf("Could not get directory absolute dir %v", err), http.StatusInternalServerError)
+		return err
+	}
+
+	absPath, err := filepath.Abs(s.Path)
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("Could not get directory absolute path %v", err))
+		http.Error(w, fmt.Sprintf("Could not get directory absolute path %v", err), http.StatusInternalServerError)
+		return err
+	}
+
+	relPath, err := filepath.Rel(absDir, absPath)
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("Could not get directory rel path %v", err))
+		http.Error(w, fmt.Sprintf("Could not get directory rel path %v", err), http.StatusInternalServerError)
+		return err
+	}
+
+	absDirName := absDir
+	if strings.HasPrefix(absDir, configs.HOME) {
+		absDirName = strings.ReplaceAll(absDir, configs.HOME, "~")
+	}
+
+	// write buffer
+	var wb bytes.Buffer
+	wb.WriteString(fmt.Sprintf("<div class=\"nav\"><span><a href=\"/\">%s</a></span>", absDirName))
+
+	relPathPwd := "."
+	splitedPaths := splitPath(relPath)
+	var ps []string
+	for k, p := range splitedPaths {
+		ps = append(ps, p)
+		if p != relPathPwd {
+			if s.Directory == "/" && k == 0 {
+				wb.WriteString(fmt.Sprintf("<span> </span><span><a href=\"/%s/\">%s</a></span>", strings.Join(ps, "/"), p))
+			} else {
+				wb.WriteString(fmt.Sprintf("<span>/</span><span><a href=\"/%s/\">%s</a></span>", strings.Join(ps, "/"), p))
+			}
 		}
 	}
-	return http.ListenAndServe(s.Addr, nil)
+	wb.WriteString("</div><ul>")
+	for _, fi := range files {
+		if fi.IsDir {
+			wb.WriteString(fmt.Sprintf("<li><span><a href=\"%s/\">%s/</a></span><span class=\"size\">%s</span><span class=\"modTime\">%s</span></li>", url.PathEscape(fi.Name), fi.Name, fi.SizeString, fi.ModTimeString))
+		} else {
+			wb.WriteString(fmt.Sprintf("<li><span><a href=\"%s\">%s</a></span><span class=\"size\">%s</span><span class=\"modTime\">%s</span></li>", url.PathEscape(fi.Name), fi.Name, fi.SizeString, fi.ModTimeString))
+		}
+	}
+	wb.WriteString("</ul>")
+
+	// write io
+	io.WriteString(w, wb.String())
+	return err
 }
 
-func (s *Server) routeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s: %s %s", r.RemoteAddr, r.Method, r.Host+r.RequestURI)
-
-	if r.Method == http.MethodGet {
-		s.handler(w, r)
-	}
+func (s *Server) renderFile(f *os.File, w http.ResponseWriter, r *http.Request) error {
+	http.ServeContent(w, r, s.Path, time.Time{}, f)
+	return nil
 }
 
-func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "no-store")
-
-	urlPath, err := url.PathUnescape(r.URL.Path)
-	s.Query = r.URL.Query()
-	if err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("failed to path unescape request path %v", err))
-		http.Error(w, fmt.Sprintf("failed to path unescape request path %v", err), http.StatusInternalServerError)
-		return
-	}
-	s.Path = path.Join(s.Directory, urlPath)
-
-	if !isExistPath(s.Path) {
-		os.Stderr.WriteString(fmt.Sprintf("No such file or directory %s", s.Path))
-		http.Error(w, "file not found", http.StatusNotFound)
-		return
-	}
-
+func (s *Server) render(w http.ResponseWriter, r *http.Request) {
+	// check path exsits
 	f, err := os.Open(s.Path)
 	defer f.Close()
 	if err != nil {
@@ -170,89 +282,27 @@ func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// render
 	if isExistDirectory(s.Path) {
-		io.WriteString(w, dirTmpl)
-		html, err := os.Open(path.Join(s.Path, "index.html"))
-
-		if err == nil {
-			io.Copy(w, html)
-			os.Stderr.WriteString(fmt.Sprintf("failed write index.html %v", err))
-			html.Close()
-			return
-		}
-		html.Close()
-
-		files, err := s.readDirectoryFiles(f)
-		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("failed read dir files %v", err))
-			http.Error(w, fmt.Sprintf("failed read dir files %v", err), http.StatusInternalServerError)
+		// render base
+		if err := s.renderBase(w); err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("failed render base %v", err))
+			http.Error(w, fmt.Sprintf("failed render base %v", err), http.StatusInternalServerError)
 			return
 		}
 
-		absDir, err := filepath.Abs(s.Directory)
-		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("Could not get directory absolute dir %v", err))
-			http.Error(w, fmt.Sprintf("Could not get directory absolute dir %v", err), http.StatusInternalServerError)
+		// render directory
+		if err = s.renderDirectory(f, w); err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("failed render directory %v", err))
+			http.Error(w, fmt.Sprintf("failed render directory %v", err), http.StatusInternalServerError)
 			return
 		}
-
-		absPath, err := filepath.Abs(s.Path)
-		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("Could not get directory absolute path %v", err))
-			http.Error(w, fmt.Sprintf("Could not get directory absolute path %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		relPath, err := filepath.Rel(absDir, absPath)
-		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("Could not get directory rel path %v", err))
-			http.Error(w, fmt.Sprintf("Could not get directory rel path %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		absDirName := absDir
-		if strings.HasPrefix(absDir, configs.HOME) {
-			absDirName = strings.ReplaceAll(absDir, configs.HOME, "~")
-		}
-
-		io.WriteString(w, fmt.Sprintf("<div class=\"nav\"><span><a href=\"/\">%s</a></span>", absDirName))
-
-		relPathPwd := "."
-		splitedPaths := splitPath(relPath)
-		var ps []string
-		for k, p := range splitedPaths {
-			ps = append(ps, p)
-			if p != relPathPwd {
-				if s.Directory == "/" && k == 0 {
-					io.WriteString(w, fmt.Sprintf("<span> </span><span><a href=\"/%s/\">%s</a></span>", strings.Join(ps, "/"), p))
-				} else {
-					io.WriteString(w, fmt.Sprintf("<span>/</span><span><a href=\"/%s/\">%s</a></span>", strings.Join(ps, "/"), p))
-				}
-			}
-		}
-		io.WriteString(w, "</div>")
-
-		io.WriteString(w, "<ul>")
-		for _, fi := range files {
-			if fi.IsDir {
-				io.WriteString(w, fmt.Sprintf("<li><span><a href=\"%s/\">%s/</a></span><span class=\"size\">%s</span><span class=\"modTime\">%s</span></li>", url.PathEscape(fi.Name), fi.Name, fi.SizeString, fi.ModTimeString))
-			} else {
-				io.WriteString(w, fmt.Sprintf("<li><span><a href=\"%s\">%s</a></span><span class=\"size\">%s</span><span class=\"modTime\">%s</span></li>", url.PathEscape(fi.Name), fi.Name, fi.SizeString, fi.ModTimeString))
-			}
-		}
-		io.WriteString(w, "</ul>")
 	} else {
-		html, err := os.Open(path.Join(s.Path, "index.html"))
-
-		if err == nil {
-			io.Copy(w, html)
-			os.Stderr.WriteString(fmt.Sprintf("failed write index.html %v", err))
-			html.Close()
+		if err = s.renderFile(f, w, r); err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("failed render file %v", err))
+			http.Error(w, fmt.Sprintf("failed render file %v", err), http.StatusInternalServerError)
 			return
 		}
-
-		html.Close()
-		http.ServeContent(w, r, s.Path, time.Time{}, f)
 	}
 }
 
